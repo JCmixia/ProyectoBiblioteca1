dc9e845d413d33b8acae1b1b5e8b40b6834fed80
Isai <Isai@DESKTOP-AT670TT>
2023-09-28 22:37:29 -0600

    Agregando cambios

---
+++ src/main/java/umg/edu/gt/insertardatos/InsertarResource.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/GenericResource.java to edit this template
+ */
+package umg.edu.gt.insertardatos;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Isai
+ */
+@Path("Insertar")
+public class InsertarResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of InsertarResource
+     */
+    public InsertarResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of umg.edu.gt.insertardatos.InsertarResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getText() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of InsertarResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void putText(String content) {
+    }
+}
