5304c2087a164ffadef46fb889182eff2257705d
Isai <Isai@DESKTOP-AT670TT>
2023-10-01 16:40:45 -0600

    Se agregaron cambios

--- src/main/java/umg/edu/gt/insertardatos/InsertarResource.java
+++ src/main/java/umg/edu/gt/insertardatos/InsertarResource.java
@@ -4,6 +4,7 @@
  */
 package umg.edu.gt.insertardatos;
 
+import com.google.gson.JsonObject;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -11,6 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -34,6 +36,29 @@ public class InsertarResource {
      * Retrieves representation of an instance of umg.edu.gt.insertardatos.InsertarResource
      * @return an instance of java.lang.String
      */
+    @GET
+    @Path("primerWS")
+    @Produces({"text/plain"})
+    @Consumes({"text/plain"})
+    public String primerWS(){
+        //TODO return proper representation object
+        return "Mi primer API WS";
+    }
+    
+    
+    @Path("pruebaWSParametrosJson")
+    @GET
+    @Consumes({"text/plain"})
+    @Produces({"text/plain"})
+    public String pruebaWSParametrosJson(@QueryParam("Nombre") String pNombre, @QueryParam("Apellido") String pApellido) {
+        JsonObject jo = new JsonObject();
+        jo.addProperty("Nombre", pNombre);
+        jo.addProperty("Apellido", pApellido);
+        System.out.println("el objeto json es: "+jo.toString());
+
+        return jo.toString();
+    }
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getText() {
